6b050fd725b0996eeafdbb6028f51e9b60b91c93
Bora Tunca <bora@soundcloud.com>
2016-01-11 23:25:39 +0100

    Add dummy finagle server

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "str-sca-cont"
 version := "1.0"
 
 scalaVersion := "2.11.7"
+
+libraryDependencies += "com.twitter" %% "finagle-http" % "6.31.0"
--- src/main/scala/MostContestedApi.scala
+++ src/main/scala/MostContestedApi.scala
@@ -1,7 +1,20 @@
 package MostContestedApi
 
-object MostContestedApi extends App{
+import com.twitter.finagle.{Http, Service, http}
+import com.twitter.util.{Await, Future}
 
-println("I am alive")
+object MostContestedApi extends App {
 
+  println("starting web server")
+
+  val service = new Service[http.Request, http.Response] {
+    def apply(req: http.Request): Future[http.Response] = {
+      val resp = http.Response(req.version, http.Status.Ok)
+      resp.setContentString("I am alive as a web server")
+      Future.value(resp)
+    }
+  }
+
+  val server = Http.serve(":8080", service)
+  Await.ready(server)
 }
